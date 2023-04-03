269ae29620cc8ebcf7202743bbd872f2e0ac0d20
Michelle Heatherly <michelleheatherly218@gmail.com>
2023-04-02 22:45:16 -0800

    just add build files

---
+++ src_assets_css_custom_scss.bundle.js
@@ -0,0 +1,2 @@
+/*! For license information please see src_assets_css_custom_scss.bundle.js.LICENSE.txt */
+"use strict";(self.webpackChunkmichelle_updated_website=self.webpackChunkmichelle_updated_website||[]).push([["src_assets_css_custom_scss"],{"./src/assets/css/custom.scss":(s,e,c)=>{c.r(e)}}]);
