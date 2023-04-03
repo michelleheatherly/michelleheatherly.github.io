5df16279f36f577094f1781fe10a357fd94a5936
Michelle Heatherly <michelleheatherly218@gmail.com>
2023-04-02 22:39:15 -0800

    add built files

---
+++ dist/src_assets_fonts_Play_css.bundle.js
@@ -0,0 +1,2 @@
+/*! For license information please see src_assets_fonts_Play_css.bundle.js.LICENSE.txt */
+"use strict";(self.webpackChunkmichelle_updated_website=self.webpackChunkmichelle_updated_website||[]).push([["src_assets_fonts_Play_css"],{"./src/assets/fonts/Play.css":(s,e,t)=>{t.r(e)}}]);
